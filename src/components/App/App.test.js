fa4dc73f542c6dce5bc79c96608c4ec78bf7592b
Scott Schipke <swschipke@gmail.com>
2019-10-03 15:11:41 -0600

    Start testing for App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import local from '../../data/local'
+import health from '../../data/health'
 
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('App', () => {
+  let event = { target: { name: 'health', value: 'g' } };
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper= shallow(<App 
+    
+    />)
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+  it('should change its current state using the event', () => {
+    expect(wrapper.state('current')).toEqual(local)
+    wrapper.instance().handleButton(event);
+    expect(wrapper.state('current')).toEqual(health)
+  });
+  it('should search stories based on a string', () => {
+    let searchTerm = 'giant';
+
+  })
+})
